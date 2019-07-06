86ccc5c34018d2b5896bb39b1e69066834dc905a
albert patterson <apatterson189@gmail.com>
2019-07-06 10:04:44 -0400

    remove unneed code and update gulp config

--- karma.conf.js
+++ karma.conf.js
@@ -72,8 +72,7 @@ module.exports = function(config) {
     // start these browsers
     // available browser launchers:
     // https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
-
+    browsers: ['ChromeHeadless', 'FirefoxHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
--- src/popup/test/popupSpec.ts
+++
@@ -1,9 +0,0 @@
-import 'mocha';
-
-import {expect} from 'chai';
-
-describe('popup module', () => {
-  it('should have a passing get', () => {
-    expect(document).not.to.be.undefined;
-  });
-});
